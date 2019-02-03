a6463164a7edc3b0aeae06478dc35401f47b9727
linxiaoyuan <linxiaoyuan@bytedance.com>
2019-02-03 16:57:56 +0800

    add 10 note

--- 10-32bit-enter/32bit-main.asm
+++ 10-32bit-enter/32bit-main.asm
@@ -9,9 +9,66 @@
     jmp $ ; this will actually never be executed
 
 %include "../05-bootsector-functions-strings/boot_sect_print.asm"
-%include "../09-32bit-gdt/32bit-gdt.asm"
 %include "../08-32bit-print/32bit-print.asm"
-%include "32bit-switch.asm"
+
+gdt_start: ; don't remove the labels, they're needed to compute sizes and jumps
+    ; the GDT starts with a null 8-byte
+    dd 0x0 ; 4 byte
+    dd 0x0 ; 4 byte
+
+; GDT for code segment. base = 0x00000000, length = 0xfffff
+; for flags, refer to os-dev.pdf document, page 36
+gdt_code: 
+    dw 0xffff    ; segment length, bits 0-15
+    dw 0x0       ; segment base, bits 0-15
+    db 0x0       ; segment base, bits 16-23
+    db 10011010b ; flags (8 bits)
+    db 11001111b ; flags (4 bits) + segment length, bits 16-19
+    db 0x0       ; segment base, bits 24-31
+
+; GDT for data segment. base and length identical to code segment
+; some flags changed, again, refer to os-dev.pdf
+gdt_data:
+    dw 0xffff
+    dw 0x0
+    db 0x0
+    db 10010010b
+    db 11001111b
+    db 0x0
+
+gdt_end:
+
+; GDT descriptor
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1 ; size (16 bit), always one less of its true size
+    dd gdt_start ; address (32 bit)
+
+; define some constants for later use
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+[bits 16]
+switch_to_pm:
+    cli ; 1. disable interrupts
+    lgdt [gdt_descriptor] ; 2. load the GDT descriptor
+    ;mov eax, cr0
+    or cr0, 0x1 ; 3. set 32-bit mode bit in cr0
+    ;mov cr0, eax
+    jmp 0x08:init_pm ; 4. far jump by using a different segment
+
+[bits 32]
+init_pm: ; we are now using 32-bit instructions
+    mov ax, DATA_SEG ; 5. update the segment registers
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    mov ebp, 0x90000 ; 6. update the stack right at the top of the free space
+    mov esp, ebp
+
+    call BEGIN_PM ; 7. Call a well-known label with useful code
 
 [bits 32]
 BEGIN_PM: ; after the switch we will get here
--- 10-32bit-enter/32bit-switch.asm
+++ 10-32bit-enter/32bit-switch.asm
@@ -5,7 +5,7 @@ switch_to_pm:
     mov eax, cr0
     or eax, 0x1 ; 3. set 32-bit mode bit in cr0
     mov cr0, eax
-    jmp CODE_SEG:init_pm ; 4. far jump by using a different segment
+    jmp init_pm ; 4. far jump by using a different segment
 
 [bits 32]
 init_pm: ; we are now using 32-bit instructions
